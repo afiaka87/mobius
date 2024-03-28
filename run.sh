cb6b8e5dc7ef5bf23cfa0b0e723b9b6d858bb5af
Clay Mullis <claymullis@fastmail.com>
2024-03-28 11:43:39 -0500

    Remove logfile behavior from run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 source .env
 
-export LD_LIBRARY_PATH=`.venv/bin/python -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
-
-.venv/bin/python claxy.py | tee "./logs/$(date +%F-%H-%M-%S)-logfile.txt"
+.venv/bin/python mobius.py
