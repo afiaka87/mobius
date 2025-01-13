c3b192b6cbd1c1ded5ed893631a59baeb7d5bd06
Clay Mullis <claymullis@fastmail.com>
2025-01-13 17:36:09 -0600

    Deleted old app scaffolding which was over-engineered.

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
+from dotenv import load_dotenv  # python-dotenv
+
+load_dotenv()  # TODO: if env errors happen, check if this is the right place to load the env
+
 import inspect
-import os
-from typing import Coroutine, Any
 import logging
+import os
+from typing import Any, Coroutine
+
+import commands as my_commands  # avoid collision with discord.ext.commands
 
 import discord
-from discord.ext import commands
 from discord import app_commands
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-from commands import image_generation, audio, text, utility, video, ollama_command
-
+from discord.ext import commands
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -24,27 +23,28 @@ intents.message_content = True
 # Initialize bot
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Define guild IDs from hard-coded environment variables
-GUILD_IDS = [
-    discord.Object(id=os.environ["DISCORD_GUILD_ID_FUNZONE"]),
-    discord.Object(id=os.environ["DISCORD_GUILD_ID_BOTTEST"]),
-]
-
 
 # Register commands
 def register_commands(bot) -> None:
     """
     Register all app commands from the command modules.
     """
-    command_modules = [image_generation, audio, text, utility, video, ollama_command]
+    command_modules = [my_commands]
 
     for module in command_modules:
-        for name, obj in inspect.getmembers(module):
+        for _, obj in inspect.getmembers(module):
             if isinstance(obj, app_commands.Command) or isinstance(
                 obj, app_commands.ContextMenu
             ):
                 print(f"Adding app command: {obj.name}")
                 bot.tree.add_command(obj)
+            else:
+                print(f"Skipping {obj}")
+
+    # If none of the commands were registered, raise an error. Something is wrong.
+    if len(bot.tree.get_commands()) == 0:
+        logging.error("No app commands registered.")
+        raise Exception("No app commands registered.")
 
     print(f"Registered {len(bot.tree.get_commands())} app commands.")
 
@@ -53,6 +53,11 @@ def sync_commands_to_guilds(bot: commands.Bot) -> Coroutine[Any, Any, None]:
     """
     Sync commands to the specified guilds.
     """
+    # Define guild IDs from hard-coded environment variables
+    GUILD_IDS = [
+        discord.Object(id=os.environ["DISCORD_GUILD_ID_FUNZONE"]),
+        discord.Object(id=os.environ["DISCORD_GUILD_ID_BOTTEST"]),
+    ]  # TODO: should be a single env variable with comma-separated guild IDs
 
     async def sync():
         for guild in GUILD_IDS:
@@ -63,6 +68,7 @@ def sync_commands_to_guilds(bot: commands.Bot) -> Coroutine[Any, Any, None]:
                 logging.info(f"Successfully synced commands to guild: {guild.id}")
             except Exception as e:
                 logging.error(f"Failed to sync commands to guild {guild.id}: {str(e)}")
+                raise e
 
     return sync()
 
@@ -79,12 +85,6 @@ async def on_ready():
     logging.info("Slash commands synced! Ready to go!")
 
 
-def run_bot():
-    """
-    Run the bot.
-    """
-    bot.run(os.environ["DISCORD_API_TOKEN"])
-
-
 if __name__ == "__main__":
-    run_bot()
+    # Load environment variables
+    bot.run(os.environ["DISCORD_API_TOKEN"])
--- scripts/scaffold-gradio.sh
+++
@@ -1,8 +0,0 @@
-mkdir gradio_app
-touch gradio_app/__init__.py
-touch gradio_app/main.py
-touch gradio_app/image_generation.py
-touch gradio_app/audio.py
-touch gradio_app/text.py
-touch gradio_app/utility.py
-touch gradio_app/video.py
